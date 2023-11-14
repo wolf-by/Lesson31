055464d7187f5080cb40ec65fe6d4d4cb9c4d4ee
wolf-by <wolf.by@yandex.ru>
2023-11-14 15:55:28 +0300

    Add migrate Products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_10_121534) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_124910) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
